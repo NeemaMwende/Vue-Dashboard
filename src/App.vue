8cafab552ff8e00e43c6ebfa82e9f07de797c77c
NeemaMwende <gracepete12@gmail.com>
2025-03-06 14:33:17 +0300

    tailwind configuratin

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="min-h-screen bg-gray-100 p-8">
+    <h1 class="text-3xl font-bold text-gray-800">Dashboard</h1>
+    <p class="mt-2 text-gray-600">Your dashboard is set up with Tailwind CSS!</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
+<!-- <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +21,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
